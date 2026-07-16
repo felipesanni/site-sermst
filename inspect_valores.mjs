0be4fc4f81639e1bcd28472e5c94598d2fc08805
Felipe <felipesannino@hotmail.com>
2026-07-16 19:54:30 -0300

    deploy

--- inspect_valores.mjs
+++
@@ -1,5 +0,0 @@
-import { FileBlob, SpreadsheetFile } from '@oai/artifact-tool';
-const input = await FileBlob.load('C:\\Users\\Felipe.Sannino\\Downloads\\valores.xlsx');
-const wb = await SpreadsheetFile.importXlsx(input);
-console.log((await wb.inspect({kind:'sheet', include:'id,name'})).ndjson);
-console.log((await wb.inspect({kind:'workbook,sheet,table', maxChars:12000, tableMaxRows:40, tableMaxCols:12, tableMaxCellChars:120})).ndjson);
